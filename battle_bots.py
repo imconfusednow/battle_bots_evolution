410c1a0ccff972eeeaf9baa4f20161d7f3dc53de
Ben Allport <ben_e_allport@hotmail.co.uk>
2019-08-04 15:59:31 +0100

    adding state system

--- battle_bots.py
+++ battle_bots.py
@@ -7,29 +7,117 @@ from neat import DataConf
 from bot import Bot
 import csv
 import pygame
+import numpy
 
+states = ["Menu", "Train", "Config", "Human", "data" ]
+game_state = 0
+
+pop_size = 200
 bots = []
-pop = Population(1, 5, 4, False)
+pop = Population(pop_size, 6, 4, False)
 nets = pop.get_pop()
 
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((1000,1000))
+data_handler = DataConf(win)
 
-bots.append(Bot(win, 100, 100, 1, nets[0], 4, False, (255, 0, 0),1))
+def make_pop():
+	for i in range(len(nets)):
+		bots.append(Bot(win, numpy.random.randint(1000), numpy.random.randint(1000), 1, nets[i], 4, (255, 0, 0),1))
+		nets[i].fitness = 1
+	data_handler.draw_net(nets[-1].dna)
+	pygame.time.wait(500)
 
 loop = True
 
-while loop:	
+def gui(game_state):
+	pressed = pygame.mouse.get_pressed()
+	if pressed[0]:
+		make_pop()
+		game_state = 1
+		with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='w', newline='') as csv_file:
+			csv_file = csv.writer(csv_file, delimiter=',')
+			csv_file.writerow(["total", "members", "fitness"])
+	return game_state
+
+def battle(game_state, count):
+	global bots
+	global nets	
+	sum_of = 0
+	if count > 1000:		
+		for bot in range(len(bots) - 1, -1, -1):
+			sum_of += bots[bot].brain.fitness
+		print(sum_of)
+		'''arr = [""] * pop.max_species_id * 2 + 1
+		arr[0] = sum_of
+
+		for s in pop.species:
+			arr[s.id * 2 + 1] = len(s.members)
+			arr[s.id * 2 + 2] = s.fitness'''
+		arr = [sum_of]
+
+		with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='a', newline='') as csv_file:
+			csv_file = csv.writer(csv_file, delimiter=',')
+			csv_file.writerow(arr)
+		nets = pop.next_gen(nets)
+		print("Pop size: ",len(nets))
+		bots = []
+		make_pop()
+		return 0
+	for bot in range(len(bots) - 1, -1, -1):
+		if not bots[bot].alive:
+			continue
+		draw = False
+		if bot < 10:
+			draw = True
+		fit = bots[bot].go(False, draw)
+		bots[bot].brain.fitness += fit
+
+	return count + 1
+
+
+
+def config(game_state):
+	pass
+
+def play(game_state):
+	bots[0].go(True, True)
+
+def calc_state(state, count):
+	if state == 1 and count > 10000:
+		count = 0
+		return 0
+	else:
+		return 1
+
+
+count = 0
+
+
+while loop:
+	
+	#game_state = calc_state(game_state, count)
+
+	if game_state == 0:
+		game_state = gui(game_state)
+	elif game_state == 1:		
+		count = battle(game_state, count)
+	elif game_state == 2:
+		config(game_state)
+	elif game_state == 3:
+		play(game_state)
+
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			loop = False
-	
+
+	pygame.display.update()
+
+	win.fill((0))
 
 
 
-	win.fill((255,255,255))
-	bots[0].go()
 
 
--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pygame
 import math
 
 class Bot:
-	def __init__(self, win, x, y, id, brain, ins, human, colour, opponent ):
+	def __init__(self, win, x, y, id, brain, ins, colour, opponent ):
 		self.win = win
 		self.x = x
 		self.y = y
@@ -12,62 +12,69 @@ class Bot:
 		self.hspeed = 0
 		self.vspeed = 0
 		self.rspeed = 0
-		self.maxspeed = 0.2
+		self.maxspeed = 3.0
 		self.maxrspeed = 10
 		self.friction = 0.001
 		self.id = id
 		self.brain = brain
 		self.alive = True
 		self.thrusting = False
-		self.thrust = 0.1
+		self.thrust = 0.0
 		self.turn_left = 0
 		self.turn_right = 0
 		self.eyes = ins
-		self.human = human
+		self.eye_pos = []
 		self.bullets = []
 		self.colour = colour
 		self.last_shot = 0
 		self.win_width, self.win_height = pygame.display.get_surface().get_size()
 		self.opponent = opponent
+		self.boundaries = [[0,0,self.win_width, 0], [self.win_width, 0, self.win_width, self.win_height], [0, self.win_height, self.win_width, self.win_height], [0, 0, 0, self.win_height]]
+		self.inputs = [100,100,100,100, self.dir, 1]
 
 
-	def go(self):
-		if self.human:
+	def go(self, human, draw):
+		if human:
 			self.get_input()
 		else:
 			self.think()
-		self.update()
-		self.draw()
-
+		to_return = self.update()
+		if draw:
+			self.draw()
+		return to_return
 
 
 	def get_input(self):
 		pressed = pygame.key.get_pressed()
 		if pressed[pygame.K_LEFT]:
-			self.left()
+			self.left(1)
 		if pressed[pygame.K_RIGHT]:
-			self.right()
+			self.right(1)
 		if pressed[pygame.K_UP]:
-			self.activate_thrust()
+			self.activate_thrust(1)
 		if pressed[pygame.K_SPACE]:
 			self.shoot()
+		self.look()
+
 
 
 	def think(self):
-		outs = self.brain.feed_forward([0,1,2,1,1])
+		self.look()
+		self.inputs[4] = self.dir / 360
+		outs = self.brain.feed_forward(self.inputs)
 		out_left = outs[0]
 		out_right = outs[1]
 		out_thrust = outs[2]
 		out_shoot = outs[3]
 
 		if out_left > 0:
-			self.left()
+			self.left(outs[0])
 		if out_right > 0:
-			self.right()
+			self.right(outs[1])
 		if out_thrust > 0:
-			self.activate_thrust()
-		if out_shoot  > 0:
-			self.shoot()
+			self.activate_thrust(outs[2])
+		#if out_shoot  > 0:
+		#	self.shoot()
 
 	def update(self):	
 		speed = numpy.sqrt(self.hspeed**2 + self.vspeed**2)
@@ -94,34 +101,44 @@ class Bot:
 		elif self.dir <= 0:
 			self.dir = 360
 
-
 		self.turn_left = 0
 		self.turn_right = 0
 		self.thrusting = False
 
-		self.update_bullets()
+		if self.x < 0 or self.x > self.win_width or self.y < 0 or self.y > self.win_height:
+			self.alive = False
+
+		#self.update_bullets()
+		return speed - abs(self.rspeed)
 
 
-	def draw(self):
+	def draw(self):				
+		for e in range(len(self.eye_pos)):
+			colour = (0, 255, 0)
+			i = self.eye_pos[e]
+			if self.inputs[e] < 1:
+				colour = (255, 0, 0)
+			pygame.draw.line(self.win, colour, (int(self.x), int(self.y)), (i[0], i[1]), 2)
 		pygame.draw.circle(self.win, self.colour, (int(self.x), int(self.y)), 20)
-		for i in range(self.eyes):
-			this_dir = self.dir + (360 / self.eyes) * i
-			pygame.draw.line(self.win, self.colour, (int(self.x), int(self.y)), (self.x + (int(math.cos(numpy.deg2rad(this_dir)) * 100)), self.y + int((math.sin(numpy.deg2rad(this_dir)) * 100))),2)
-		pygame.display.update()
+		for bu in range(len(self.bullets) - 1, -1, -1):
+			b = self.bullets[bu]
+			b.draw()		
 
-	def activate_thrust(self):
+
+	def activate_thrust(self, power):
+		self.thrust = power
 		self.thrusting = True
 
-	def left(self):
-		self.turn_left = 0.1
+	def left(self, power):
+		self.turn_left = power
 
-	def right(self):
-		self.turn_right = 0.1
+	def right(self, power):
+		self.turn_right = power
 
 
 	def shoot(self):
 		if self.last_shot > 200:
-			self.bullets.append(Bullet(self.win, self.x, self.y, math.cos(numpy.deg2rad(self.dir)), math.sin(numpy.deg2rad(self.dir)), self.colour))
+			self.bullets.append(Bullet(self.win, self.x, self.y, math.cos(numpy.deg2rad(self.dir)), math.sin(numpy.deg2rad(self.dir)), (255,255,0)))
 			self.last_shot = 0
 		else:
 			self.last_shot += 1
@@ -132,5 +149,59 @@ class Bot:
 			if b.age > 2000:
 				del self.bullets[bu]
 			b.update()
-			b.draw()
+			
+
+	def look(self):
+		self.eye_pos = []
+		for i in range(self.eyes):
+			this_dir = self.dir + (360 / self.eyes) * i
+			end = [self.x + int(math.cos(numpy.deg2rad(this_dir)) * 100), self.y + int(math.sin(numpy.deg2rad(this_dir)) * 100)]
+			self.eye_pos.append(end)
+
+		for j in range(len(self.eye_pos)):
+			record = 99999
+			closest = []
+			for i in range(len(self.boundaries)):
+				d = self.cast(self.boundaries[i],self.eye_pos[j])
+				if ( d < record ):
+					record = d
+			self.eye_pos[j][0] = (self.eye_pos[j][0] - self.x) / 100
+			self.eye_pos[j][1] = (self.eye_pos[j][1] - self.y) / 100
+			self.eye_pos[j][0] = self.eye_pos[j][0]  * record + self.x
+			self.eye_pos[j][1] = self.eye_pos[j][1]  * record + self.y
+			self.inputs[j] = record / 100
+		
+
+
+
+	def cast(self, wall, eye):
+		x1 = wall[0]
+		y1 = wall[1]
+		x2 = wall[2]
+		y2 = wall[3]
+
+		x3 = self.x
+		y3 = self.y
+		x4 = eye[0]
+		y4 = eye[1]
+
+		den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+		if (den == 0):
+			return 101
+
+		t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+		u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+		if (t > 0 and t < 1 and u > 0):
+			pt = []
+			pt.append(x1 + t * (x2 - x1))
+			pt.append(y1 + t * (y2 - y1))
+			dist = numpy.sqrt((self.x - pt[0])**2 + (self.y - pt[1])**2)
+			if (dist > 100):
+				return 100
+			else:
+				return dist
+		else:
+			return 101
+
+
 
