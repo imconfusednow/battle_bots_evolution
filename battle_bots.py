5b1b2211f04c96fa3ee65ec03e8e82741477b36f
Ben Allport <ben_e_allport@hotmail.co.uk>
2019-12-17 22:40:43 +0000

    commiting changes

--- battle_bots.py
+++ battle_bots.py
@@ -1,123 +1,187 @@
+import numpy
+import pygame
+import csv
+from bot import Bot
 import sys
 sys.path.insert(0, 'H:\\Documents\\Coding_projects')
 sys.path.insert(1, 'H:\\Documents\\Coding_projects\\neat')
-from neat import Population
-from neat import Network
 from neat import DataConf
-from bot import Bot
-import csv
-import pygame
-import numpy
+from neat import Network
+from neat import Population
 
-states = ["Menu", "Train", "Config", "Human", "data" ]
+states = ["Menu", "Train", "Config", "Human", "data", "show"]
 game_state = 0
 
-pop_size = 200
+pop_size = 400
 bots = []
-pop = Population(pop_size, 6, 4, False)
+pop = Population(pop_size, 5, 3, True)
 nets = pop.get_pop()
 
 pygame.init()
 
-win = pygame.display.set_mode((1000,1000))
+win_width = 1000
+win_height = 1000
+
+win = pygame.display.set_mode((win_width, win_height))
 data_handler = DataConf(win)
 
+
 def make_pop():
-	for i in range(len(nets)):
-		bots.append(Bot(win, numpy.random.randint(1000), numpy.random.randint(1000), 1, nets[i], 4, (255, 0, 0),1))
-		nets[i].fitness = 1
-	data_handler.draw_net(nets[-1].dna)
-	pygame.time.wait(500)
+    print("Creating population of " + str(len(nets)))
+    complexity = 0
+    for i in range(len(nets)):
+        bots.append(Bot(win, numpy.random.randint(win_width),
+                        numpy.random.randint(win_height), 1, nets[i], 4, (255, 0, 0), 1))
+        nets[i].fitness = 1
+        complexity += nets[i].max_node
+
+    print("Complexity: ", complexity)
+
 
 loop = True
 
+
 def gui(game_state):
-	pressed = pygame.mouse.get_pressed()
-	if pressed[0]:
-		make_pop()
-		game_state = 1
-		with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='w', newline='') as csv_file:
-			csv_file = csv.writer(csv_file, delimiter=',')
-			csv_file.writerow(["total", "members", "fitness"])
-	return game_state
+    pressed = pygame.mouse.get_pressed()
+    if pressed[0]:
+        make_pop()
+        '''dna = {'neurons': [[0, 1, 2, 3, 4], [5, 6, 7]], 'connections': [[0, 5, 1, True], [0, 6, 0.0, True], [0, 7, -1, True], [1, 5, 1, True], [1, 6, -0.8, True], [1, 7, 0.0, True], [2, 5, -0.0, True], [2, 6, 0.0, True], [2, 7, 0.1, True], [3, 5, -0.8, True], [3, 6, 1, True], [3, 7, 0.0, True], [4, 5, -0.0, True], [4, 6, 0.0, True], [4, 7, 0.1, True]], 'recurrents': []}
+        nets.append(Network(5, 3, True, 0, dna, False, False, 0, (255, 255, 0)))
+        bots[0] = (Bot(win, numpy.random.randint(win_width), numpy.random.randint(win_height), 1, nets[-1], 4, (255, 255, 0), 1))'''
+        game_state = 1
+        with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='w', newline='') as csv_file:
+            csv_file = csv.writer(csv_file, delimiter=',')
+            csv_file.writerow(["total", "Best", "Theoretical"])
+    return game_state
+
+
+theoretical = 0
+theoretical_adj = 0
+
 
 def battle(game_state, count):
-	global bots
-	global nets	
-	sum_of = 0
-	if count > 1000:		
-		for bot in range(len(bots) - 1, -1, -1):
-			sum_of += bots[bot].brain.fitness
-		print(sum_of)
-		'''arr = [""] * pop.max_species_id * 2 + 1
-		arr[0] = sum_of
+    global bots
+    global nets
+    global theoretical
+    global theoretical_adj
+    sum_of = 0
+    best = 0
 
-		for s in pop.species:
-			arr[s.id * 2 + 1] = len(s.members)
-			arr[s.id * 2 + 2] = s.fitness'''
-		arr = [sum_of]
+    if count > 1500:
 
-		with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='a', newline='') as csv_file:
-			csv_file = csv.writer(csv_file, delimiter=',')
-			csv_file.writerow(arr)
-		nets = pop.next_gen(nets)
-		print("Pop size: ",len(nets))
-		bots = []
-		make_pop()
-		return 0
-	for bot in range(len(bots) - 1, -1, -1):
-		if not bots[bot].alive:
-			continue
-		draw = False
-		if bot < 10:
-			draw = True
-		fit = bots[bot].go(False, draw)
-		bots[bot].brain.fitness += fit
+        for bot in range(len(bots) - 1, -1, -1):
+            sum_of += bots[bot].brain.fitness
+            if bots[bot].brain.fitness > best:
+                best = bots[bot].brain.fitness
+        print(sum_of)
+        '''arr = [""] * pop.max_species_id * 2 + 1
+        arr[0] = sum_of
 
-	return count + 1
+        for s in pop.species:
+            arr[s.id * 2 + 1] = len(s.members)
+            arr[s.id * 2 + 2] = s.fitness'''
+        arr = [sum_of, best, theoretical, theoretical_adj]
+        theoretical = 0
+        theoretical_adj = 0
 
+        with open('H:\\Documents\\Coding_projects\\battle_bots_evolution\\data\\scores.csv', mode='a', newline='') as csv_file:
+            csv_file = csv.writer(csv_file, delimiter=',')
+            csv_file.writerow(arr)
+        nets = pop.next_gen(nets)
+        print("Pop size: ", len(nets))
+        bots = []
+        make_pop()
+        return 0
+    for bot in range(len(bots) - 1, -1, -1):
+        theoretical += 10
+        if not bots[bot].alive:
+            bots[bot].brain.fitness = 0
+            continue
+        draw = False
+        if bot < 100 and count % 3 == 0:
+            draw = True
+        fit = bots[bot].go(False, draw)
+        bots[bot].brain.fitness += fit
+        theoretical_adj += 10
+
+    return count + 1
 
 
 def config(game_state):
-	pass
+    pass
+
+
+def data(game_state):
+    data_handler.draw_brain(nets)
+
 
 def play(game_state):
-	bots[0].go(True, True)
+    if len(bots) == 0:
+        net = Network(3, 6, True, 0, {"neurons": [[], []], "connections": [], "recurrents": []}, True, False, 0)
+        bots.append(Bot(win, numpy.random.randint(win_width),
+                        numpy.random.randint(win_height), 1, net, 4, (255, 0, 0), 1))
+    bots[0].go(True, True)
+
+
+def show_one(game_state):
+    global nets
+    if len(bots) == 0:
+        dna = {'neurons': [[0, 1, 2, 3, 4], [5, 6, 7]], 'connections': [[0, 5, 1, True], [0, 6, 0.0, True], [0, 7, -1, True], [1, 5, 1, True], [1, 6, -0.8, True], [1, 7, 0.0, True], [2, 5, -0.0, True], [2, 6, 0.0, True], [2, 7, 0.1, True], [3, 5, -0.8, True], [3, 6, 1, True], [3, 7, 0.0, True], [4, 5, -0.0, True], [4, 6, 0.0, True], [4, 7, 0.1, True]], 'recurrents': []}
+        nets = [Network(5, 3, True, 0, dna, False, False, 0)]
+        bots.append(Bot(win, numpy.random.randint(win_width),
+                    numpy.random.randint(win_height), 1, nets[0], 4, (255, 0, 0), 1))
+    for bot in range(len(bots) - 1, -1, -1):
+        if not bots[bot].alive:
+            continue
+        fit = bots[bot].go(False, True)
+        bots[bot].brain.fitness += fit
+
+    print(bots[0].brain.fitness)
+
 
 def calc_state(state, count):
-	if state == 1 and count > 10000:
-		count = 0
-		return 0
-	else:
-		return 1
+    if state == 1 and count > 10000:
+        count = 0
+        return 0
+    else:
+        return 1
 
 
 count = 0
+last_state = 0
+
+def do_stuff():
+    global game_state
+    global count
+    global last_state
+
+    if game_state == 0:
+        game_state = gui(game_state)
+    elif game_state == 1:
+        count = battle(game_state, count)
+    elif game_state == 2:
+        config(game_state)
+    elif game_state == 3:
+        play(game_state)
+    elif game_state == 4:
+        data(game_state)
+        game_state = last_state
+    elif game_state == 5:
+        show_one(game_state)
+
+    if count % 4 == 0:
+        pygame.display.update()
+        win.fill((0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    last_state = game_state
+                    game_state = 4
+
+    return True
 
 
 while loop:
-	
-	#game_state = calc_state(game_state, count)
-
-	if game_state == 0:
-		game_state = gui(game_state)
-	elif game_state == 1:		
-		count = battle(game_state, count)
-	elif game_state == 2:
-		config(game_state)
-	elif game_state == 3:
-		play(game_state)
-
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			loop = False
-
-	pygame.display.update()
-
-	win.fill((0))
-
-
-
-
-
+    loop = do_stuff()
--- bot.py
+++ bot.py
@@ -3,205 +3,215 @@ import numpy
 import pygame
 import math
 
+
 class Bot:
-	def __init__(self, win, x, y, id, brain, ins, colour, opponent ):
-		self.win = win
-		self.x = x
-		self.y = y
-		self.dir = numpy.random.uniform(0,359)
-		self.hspeed = 0
-		self.vspeed = 0
-		self.rspeed = 0
-		self.maxspeed = 3.0
-		self.maxrspeed = 10
-		self.friction = 0.001
-		self.id = id
-		self.brain = brain
-		self.alive = True
-		self.thrusting = False
-		self.thrust = 0.0
-		self.turn_left = 0
-		self.turn_right = 0
-		self.eyes = ins
-		self.eye_pos = []
-		self.bullets = []
-		self.colour = colour
-		self.last_shot = 0
-		self.win_width, self.win_height = pygame.display.get_surface().get_size()
-		self.opponent = opponent
-		self.boundaries = [[0,0,self.win_width, 0], [self.win_width, 0, self.win_width, self.win_height], [0, self.win_height, self.win_width, self.win_height], [0, 0, 0, self.win_height]]
-		self.inputs = [100,100,100,100, self.dir, 1]
+    def __init__(self, win, x, y, id, brain, ins, colour, opponent):
+        self.win = win
+        self.x = x
+        self.y = y
+        self.dir = numpy.random.uniform(0, 359)
+        self.hspeed = 0
+        self.vspeed = 0
+        self.rspeed = 0
+        self.maxspeed = 3.0
+        self.maxrspeed = 0.1
+        self.friction = 0.99
+        self.id = id
+        self.brain = brain
+        self.alive = True
+        self.thrust = 0.0
+        self.turn_left = 0
+        self.turn_right = 0
+        self.eyes = ins
+        self.eye_pos = []
+        self.bullets = []
+        self.colour = colour
+        self.last_shot = 0
+        self.win_width, self.win_height = pygame.display.get_surface().get_size()
+        self.opponent = opponent
+        self.boundaries = [[0, 0, self.win_width, 0], [self.win_width, 0, self.win_width, self.win_height], [
+            0, self.win_height, self.win_width, self.win_height], [0, 0, 0, self.win_height]]
+        self.inputs = [0, 0, 0, 0, 1]
+        self.font = pygame.font.SysFont("comicsansms", 12)
+        self.age = 0
 
+    def go(self, human, draw):
+        if human:
+            self.get_input()
+        else:
+            self.think()
+        to_return = self.update()
+        if draw:
+            self.draw()
+        return to_return
 
-	def go(self, human, draw):
-		if human:
-			self.get_input()
-		else:
-			self.think()
-		to_return = self.update()
-		if draw:
-			self.draw()
-		return to_return
+    def get_input(self):
+        pressed = pygame.key.get_pressed()
+        left = 0
+        right = 0
+        if pressed[pygame.K_LEFT]:
+            self.left(1)
+            right = 1
+        if pressed[pygame.K_RIGHT]:
+            self.right(1)
+            left = 1
+        if pressed[pygame.K_UP]:
+            self.activate_thrust(1)
+        if pressed[pygame.K_SPACE]:
+            self.shoot()
+        self.look()
 
+    def think(self):
+        self.look()
+        outs = self.brain.feed_forward(self.inputs)
+        out_left = outs[0]
+        out_right = outs[1]
+        out_thrust = outs[2]
+        # out_shoot = outs[3]
+        if out_left > 0:
+            self.left(outs[0])
+        if out_right > 0:
+            self.right(outs[1])
+        if out_thrust > 0:
+            self.activate_thrust(out_thrust)
+        # if out_thrust > 0:
+        # self.activate_thrust(outs[2])
+        # if out_shoot  > 0:
+        # self.shoot()
 
-	def get_input(self):
-		pressed = pygame.key.get_pressed()
-		if pressed[pygame.K_LEFT]:
-			self.left(1)
-		if pressed[pygame.K_RIGHT]:
-			self.right(1)
-		if pressed[pygame.K_UP]:
-			self.activate_thrust(1)
-		if pressed[pygame.K_SPACE]:
-			self.shoot()
-		self.look()
+    def update(self):
+        speed = numpy.sqrt(self.hspeed**2 + self.vspeed**2)
+        if speed > 0.001:
+            self.hspeed *= self.friction
+            self.vspeed *= self.friction
+        else:
+            self.hspeed = 0
+            self.vspeed = 0
+        if self.thrust > 0:
+            if speed < self.maxspeed:
+                self.hspeed += self.thrust * math.cos(numpy.deg2rad(self.dir))
+                self.vspeed += self.thrust * math.sin(numpy.deg2rad(self.dir))
+            else:
+                self.hspeed = self.maxspeed * math.cos(numpy.deg2rad(self.dir))
+                self.vspeed = self.maxspeed * math.sin(numpy.deg2rad(self.dir))
 
+        self.rspeed = self.turn_right - self.turn_left
+        if self.rspeed > self.maxrspeed: self.rspeed = self.maxrspeed
 
+        self.x += self.hspeed
+        self.y += self.vspeed
+        self.dir += self.rspeed
+        if self.dir > 360:
+            self.dir = 0
+        elif self.dir <= 0:
+            self.dir = 360
 
-	def think(self):
-		self.look()
-		self.inputs[4] = self.dir / 360
-		outs = self.brain.feed_forward(self.inputs)
-		out_left = outs[0]
-		out_right = outs[1]
-		out_thrust = outs[2]
-		out_shoot = outs[3]
+        self.turn_left = 0
+        self.turn_right = 0
+        self.thrust = 0
 
-		if out_left > 0:
-			self.left(outs[0])
-		if out_right > 0:
-			self.right(outs[1])
-		if out_thrust > 0:
-			self.activate_thrust(outs[2])
-		#if out_shoot  > 0:
-		#	self.shoot()
+        self.age += abs(self.rspeed)
 
-	def update(self):	
-		speed = numpy.sqrt(self.hspeed**2 + self.vspeed**2)
-		if self.thrusting:
-			if speed < self.maxspeed:
-				self.hspeed += self.thrust * math.cos(numpy.deg2rad(self.dir))
-				self.vspeed += self.thrust * math.sin(numpy.deg2rad(self.dir))
-			else:
-				self.hspeed = self.maxspeed * math.cos(numpy.deg2rad(self.dir))
-				self.vspeed = self.maxspeed * math.sin(numpy.deg2rad(self.dir))
-		elif speed > 0.01:
-			self.hspeed *= self.friction
-			self.vspeed *= self.friction
-		else:
-			self.hspeed = 0
-			self.vspeed = 0
-		self.rspeed =  self.turn_right - self.turn_left
-		
-		self.x += self.hspeed
-		self.y += self.vspeed
-		self.dir += self.rspeed
-		if self.dir > 360:
-			self.dir = 0
-		elif self.dir <= 0:
-			self.dir = 360
+        if self.age > 15000 or self.x < 0 or self.x > self.win_width or self.y < 0 or self.y > self.win_height:
+            self.alive = False
 
-		self.turn_left = 0
-		self.turn_right = 0
-		self.thrusting = False
+        # self.update_bullets()
 
-		if self.x < 0 or self.x > self.win_width or self.y < 0 or self.y > self.win_height:
-			self.alive = False
+        if abs(self.rspeed) > 0.1:
+            speed = 0
+        return max(0, speed)
 
-		#self.update_bullets()
-		return speed - abs(self.rspeed)
+    def draw(self):
+        for e in range(len(self.eye_pos)):
+            colour = (0, 255, 0)
+            i = self.eye_pos[e]
+            if self.inputs[e] > 0:
+                colour = (255, 0, 0)
+            pygame.draw.line(self.win, colour, (int(self.x),
+                                                int(self.y)), (i[0], i[1]), 2)
+        colour = self.brain.colour if self.brain != 0 and self.brain.max_node < 11 else (
+            0, 0, 255)
+        pygame.draw.circle(self.win, colour, (int(self.x), int(self.y)), 20)
+        self.text(str(round(self.brain.fitness)), self.x, self.y)
+        for bu in range(len(self.bullets) - 1, -1, -1):
+            b = self.bullets[bu]
+            b.draw()
 
+    def text(self, text, x, y):
+        text = self.font.render(text, True, (0, 0, 0))
+        self.win.blit(text, (x, y))
 
-	def draw(self):				
-		for e in range(len(self.eye_pos)):
-			colour = (0, 255, 0)
-			i = self.eye_pos[e]
-			if self.inputs[e] < 1:
-				colour = (255, 0, 0)
-			pygame.draw.line(self.win, colour, (int(self.x), int(self.y)), (i[0], i[1]), 2)
-		pygame.draw.circle(self.win, self.colour, (int(self.x), int(self.y)), 20)
-		for bu in range(len(self.bullets) - 1, -1, -1):
-			b = self.bullets[bu]
-			b.draw()		
+    def activate_thrust(self, power):
+        self.thrust = power * 0.1
 
+    def left(self, power):
+        self.turn_left = power * 2
 
-	def activate_thrust(self, power):
-		self.thrust = power
-		self.thrusting = True
+    def right(self, power):
+        self.turn_right = power * 2
 
-	def left(self, power):
-		self.turn_left = power
+    def shoot(self):
+        if self.last_shot > 200:
+            self.bullets.append(Bullet(self.win, self.x, self.y, math.cos(
+                numpy.deg2rad(self.dir)), math.sin(numpy.deg2rad(self.dir)), (255, 255, 0)))
+            self.last_shot = 0
+        else:
+            self.last_shot += 1
 
-	def right(self, power):
-		self.turn_right = power
+    def update_bullets(self):
+        for bu in range(len(self.bullets) - 1, -1, -1):
+            b = self.bullets[bu]
+            if b.age > 2000:
+                del self.bullets[bu]
+            b.update()
 
+    def look(self):
+        self.eye_pos = []
+        for i in range(self.eyes):
+            this_dir = self.dir + (360 / self.eyes) * i
+            end = [self.x + int(math.cos(numpy.deg2rad(this_dir)) * 100),
+                   self.y + int(math.sin(numpy.deg2rad(this_dir)) * 100)]
+            self.eye_pos.append(end)
 
-	def shoot(self):
-		if self.last_shot > 200:
-			self.bullets.append(Bullet(self.win, self.x, self.y, math.cos(numpy.deg2rad(self.dir)), math.sin(numpy.deg2rad(self.dir)), (255,255,0)))
-			self.last_shot = 0
-		else:
-			self.last_shot += 1
+        if self.x > 100 and self.x < self.win_width - 100 and self.y > 100 and self.y < self.win_height - 100:
+            return
 
-	def update_bullets(self):
-		for bu in range(len(self.bullets) - 1, -1, -1):
-			b = self.bullets[bu]
-			if b.age > 2000:
-				del self.bullets[bu]
-			b.update()
-			
+        for j in range(len(self.eye_pos)):
+            for i in range(len(self.boundaries)):
+                d = self.cast(self.boundaries[i], self.eye_pos[j])
+                if (d < 100):
+                    break
+            self.eye_pos[j][0] = (self.eye_pos[j][0] - self.x) / 100
+            self.eye_pos[j][1] = (self.eye_pos[j][1] - self.y) / 100
+            self.eye_pos[j][0] = self.eye_pos[j][0] * d + self.x
+            self.eye_pos[j][1] = self.eye_pos[j][1] * d + self.y
+            self.inputs[j] = 1 - (d / 100)
 
-	def look(self):
-		self.eye_pos = []
-		for i in range(self.eyes):
-			this_dir = self.dir + (360 / self.eyes) * i
-			end = [self.x + int(math.cos(numpy.deg2rad(this_dir)) * 100), self.y + int(math.sin(numpy.deg2rad(this_dir)) * 100)]
-			self.eye_pos.append(end)
+    def cast(self, wall, eye):
+        x1 = wall[0]
+        y1 = wall[1]
+        x2 = wall[2]
+        y2 = wall[3]
 
-		for j in range(len(self.eye_pos)):
-			record = 99999
-			closest = []
-			for i in range(len(self.boundaries)):
-				d = self.cast(self.boundaries[i],self.eye_pos[j])
-				if ( d < record ):
-					record = d
-			self.eye_pos[j][0] = (self.eye_pos[j][0] - self.x) / 100
-			self.eye_pos[j][1] = (self.eye_pos[j][1] - self.y) / 100
-			self.eye_pos[j][0] = self.eye_pos[j][0]  * record + self.x
-			self.eye_pos[j][1] = self.eye_pos[j][1]  * record + self.y
-			self.inputs[j] = record / 100
-		
+        x3 = self.x
+        y3 = self.y
+        x4 = eye[0]
+        y4 = eye[1]
 
+        den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+        if (den == 0):
+            return 100
 
-
-	def cast(self, wall, eye):
-		x1 = wall[0]
-		y1 = wall[1]
-		x2 = wall[2]
-		y2 = wall[3]
-
-		x3 = self.x
-		y3 = self.y
-		x4 = eye[0]
-		y4 = eye[1]
-
-		den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-		if (den == 0):
-			return 101
-
-		t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
-		u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
-		if (t > 0 and t < 1 and u > 0):
-			pt = []
-			pt.append(x1 + t * (x2 - x1))
-			pt.append(y1 + t * (y2 - y1))
-			dist = numpy.sqrt((self.x - pt[0])**2 + (self.y - pt[1])**2)
-			if (dist > 100):
-				return 100
-			else:
-				return dist
-		else:
-			return 101
-
-
-
+        t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+        u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+        if (t > 0 and t < 1 and u > 0):
+            pt = []
+            pt.append(x1 + t * (x2 - x1))
+            pt.append(y1 + t * (y2 - y1))
+            dist = numpy.sqrt((self.x - pt[0])**2 + (self.y - pt[1])**2)
+            if (dist > 100):
+                return 100
+            else:
+                return dist
+        else:
+            return 100
